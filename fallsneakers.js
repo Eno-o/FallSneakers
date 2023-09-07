d4425ebb45db4c3738061872aff6e9069e2e571f
Eno-o <ojarikreblessing2000@gmail.com>
2023-09-07 18:27:08 +0100

    initial commit

---
+++ fallsneakers.js
@@ -0,0 +1,15 @@
+var MainImg = document.getElementById('MainImg');
+        var small_img = document.getElementsByClassName('small_img');
+
+        small_img[0].onclick = function() {
+            MainImg.src = small_img[0].src
+        }
+        small_img[1].onclick = function() {
+            MainImg.src = small_img[1].src
+        }
+        small_img[2].onclick = function() {
+            MainImg.src = small_img[2].src
+        }
+        small_img[3].onclick = function() {
+            MainImg.src = small_img[3].src
+        }
